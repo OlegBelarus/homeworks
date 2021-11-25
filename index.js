e5a6902a53fc34268446daf55433dfd658a9b3bc
OlegBelarus <94072338+OlegBelarus@users.noreply.github.com>
2021-11-25 17:30:16 +0300

    commit 1

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-// 1 Есть строка 55639217. переделать её в 5:563:921:7.
-//Суть задачи сводится не к изменению конкретной строки из условия,
-//а в том чтобы добавлять двоеточие между двумя нечётными числами.
+// Есть строка 55639217. переделать её в 5:563:921:7.
+// Суть задачи сводится не к изменению конкретной строки из условия,
+// а в том чтобы добавлять двоеточие между двумя нечётными числами.
 
 const checkNumb = string => {
     const str = string;
@@ -17,5 +17,26 @@ const checkNumb = string => {
     return newNumbStr.join('');
 }
 
-const ipAddress = checkNumb('55639217');
+const ipAddress = checkNumb('556996639217');
 console.log(ipAddress);
+
+//Напишите функцию countIdentic(arr), которая возвращает количество повторяющихся элементов в массиве.
+
+const arr = [2, 2, 3, 5, 11, 10, 11, 10, 69, 69];
+
+const countIdentic = arr => {
+    let result = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[i] === arr[j] && i !== j) {
+                result++;
+                arr.splice(i, 1);
+            }
+        }
+    }
+    return result;
+}
+
+console.log(countIdentic(arr));
+
