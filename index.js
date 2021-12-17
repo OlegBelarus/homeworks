b8e018c9c5fd2e817d9ab9ea424a70400c41ae8f
OlegBelarus <94072338+OlegBelarus@users.noreply.github.com>
2021-12-17 17:23:00 +0300

    commit 1

--- index.js
+++ index.js
@@ -1,12 +1,50 @@
-// number 3
+// number 1
 
-const arr = ['cherry', 'sun', 'room', 'ok'];
+const array = [
+    {
+        name: 'Vasya',
+        age: 22
+    },
+    {
+        name: 'Marina',
+        age: 27
+    }
+];
 
-const newArr = arr.reduce( (acc, item) => {
+const propertyValue = (array, key) => {
+    const newArr = [];
 
-    return acc + item.length + ' ';
-}, []);
+    array.map ( (item) => {
+        item[key];
+    });
+};
 
-    console.log(newArr);
+console.log(propertyValue(array, 'age'));
 
-//Остальное доделаю на следующий раз
+
+// // number 2
+
+// const arr = [1, 9, 10, 8, 9, 22, 9, 96];
+
+
+// const removeItem = (arr, num) => {
+
+//     const newArr = arr.filter(item => item !== num);
+
+//     return newArr;
+// };
+
+// console.log(removeItem(arr, 8));
+
+//number 3
+
+// const arr = ['cherry', 'sun', 'room', 'ok'];
+
+// const newArr = arr.reduce( (acc, item) => {
+
+//     return acc + item.length + ' ';
+// }, []);
+
+//     console.log(newArr);
+
+//number 4
