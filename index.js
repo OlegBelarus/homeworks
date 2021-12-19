8bfb1c5bde13908d6c94d2a85809f5ec67f2e80e
OlegBelarus <94072338+OlegBelarus@users.noreply.github.com>
2021-12-19 09:58:58 +0300

    commit 1

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
-// number 1
+// // number 1
 
-const array = [
-    {
-        name: 'Vasya',
-        age: 22
-    },
-    {
-        name: 'Marina',
-        age: 27
-    }
-];
+// const users = [
+//     {
+//         name: 'Vasya',
+//         age: 22
+//     },
+//     {
+//         name: 'Marina',
+//         age: 27
+//     }
+// ];
 
-const propertyValue = (array, key) => {
-    const newArr = [];
+// const propertyValue = (x, y) => x.map(item => item[y]);
 
-    array.map ( (item) => {
-        item[key];
-    });
-};
-
-console.log(propertyValue(array, 'age'));
+// console.log(propertyValue(users, 'name'));
 
 
 // // number 2
@@ -36,15 +30,34 @@ console.log(propertyValue(array, 'age'));
 
 // console.log(removeItem(arr, 8));
 
-//number 3
+// // number 3
 
 // const arr = ['cherry', 'sun', 'room', 'ok'];
 
 // const newArr = arr.reduce( (acc, item) => {
+//     // const prevVal = acc;
 
-//     return acc + item.length + ' ';
+//     // prevVal.push(item.length);
+
+//     return [...acc, item.length];
 // }, []);
 
 //     console.log(newArr);
 
-//number 4
+// number 4
+
+// const numbers = [2, 3, 5, 7, 11, 13, 17, 19];
+
+// const currentSums = array => {
+
+//     return array.reduce( (acc, item, idx) => {
+
+//         if (idx === 0) {
+//             return [item];
+//         }
+
+//         return [...acc, item + acc[idx - 1]];
+//     }, []);
+// };
+
+// console.log(currentSums(numbers));
