7af673299d7d8ca21e40115b97327817d012198f
OlegBelarus <94072338+OlegBelarus@users.noreply.github.com>
2021-12-23 02:10:14 +0300

    commit 1

--- index.js
+++ index.js
@@ -1,34 +1,31 @@
-const randomNumber = Math.floor(Math.random() * 10 + 1);
-let result = 0;
+const body = document.querySelector('body');
+const users = [
+  {
+    name: 'Olga',
+    age: 12
+  },
+  {
+    name: 'Misha',
+    age: 25
+  },
+  {
+    name: 'Petya',
+    age: 45
+  },
+  {
+    name: 'Masha',
+    age: 18
+  },
+];
 
-const butNum = document.querySelector('.butNum');
-const guessField = document.querySelector('.guessField');
+users.forEach(({name, age}) => {
+  const p = document.createElement('p');
 
-const sumInput = document.querySelector('.sumInput');
-const resultContent = document.querySelector('.resultContent');
-const strHelp = document.querySelector('.strHelp');
+  p.innerText = `Name ${name}, age ${age}`;
 
+  p.onclick = () => {
+    p.remove()
+  }
 
-const guessNumber = () => {
-    const userNumber = Number(guessField.value); 
-    result++;
-
-    sumInput.textContent = 'Попыток: ' + result;
-
-      if (userNumber === randomNumber) {
-        resultContent.textContent = 'Красавчик! Угадал! Нажимай F5, играй ещё!';
-        strHelp.textContent = '';
-      } else {
-        resultContent.textContent = 'Неудачник, пробуй ещё!';
-        if(userNumber < randomNumber) {
-            strHelp.textContent = 'Вы ввели меньше заданного числа';
-        } else if(userNumber > randomNumber) {
-            strHelp.textContent = 'Вы ввели больше заданного числа!';
-        }
-    };
-
-      guessField.value = '';
-      guessField.focus();
-};
-
-butNum.addEventListener('click', guessNumber);
+  body.appendChild(p);
+});
