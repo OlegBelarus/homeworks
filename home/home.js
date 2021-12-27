51aa5b5bc13da92ba1fa18a04c57cc1c215efc88
OlegBelarus <94072338+OlegBelarus@users.noreply.github.com>
2021-12-27 13:52:08 +0300

    commit 1

--- home/home.js
+++ home/home.js
@@ -1,7 +1,13 @@
-if (localStorage.getItem('users') === null) {
-    window.location = '../auth/auth.html';
-} else {
-    window.location = '/home/home.html';
-}
+setTimeout(() => {
+    if (localStorage.getItem('users') === null) {
+        window.location = '../auth/auth.html';
+    } else {
+        window.location = '/home/home.html';
+    }
+}, 5000);
 
-console.log(localStorage);
+const btn_clear = document.getElementById('btn_clear');
+
+btn_clear.onclick = () =>{
+    localStorage.removeItem('users');
+};
