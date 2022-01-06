a699024a921f469f8d653c33ccef84862efd96c2
OlegBelarus <94072338+OlegBelarus@users.noreply.github.com>
2022-01-06 14:21:45 +0300

    commit 1

--- auth/auth.js
+++ auth/auth.js
@@ -1,6 +1,7 @@
 const btn_auth = document.getElementById('btn_auth');
 const inp_log= document.getElementById('inp_log');
 const inp_pass = document.getElementById('inp_pass');
+const btn_reg = document.getElementById('btn_reg');
 const users = [
     {
         login: 'login01',
@@ -12,9 +13,17 @@ const users = [
     },
   ];
 
-
 btn_auth.onclick = () => {
-     users.find(({login, password}) => login === inp_log.value && password === inp_pass.value) ?
+    users.find(({login, password}) => login === inp_log.value && password === inp_pass.value) ?
     window.location = '/home/home.html':
     alert ('Please re-enter your login or password!');
+};
+
+btn_reg.onclick = () => {
+    const users = {
+        inp_log: inp_log.value,
+        inp_pass: inp_pass.value
+    };
+    localStorage.setItem('users', JSON.stringify(users));
+    window.location = '/home/home.html';
 };
